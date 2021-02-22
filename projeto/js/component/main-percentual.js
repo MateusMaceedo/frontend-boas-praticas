797fed3fcffd42a1bb51bf0d065986b37fd7a4d3
mateusmacedo <mateusouza2014@live.com>
2021-02-21 22:28:00 -0300

    nova version do CSS e HTML

--- projeto/js/component/main-percentual.js
+++ projeto/js/component/main-percentual.js
@@ -1,5 +1,9 @@
+// pegar o container do texto
+const body = document.querySelector(".body")
+
 // cria a barrinha
 let bar = document.createElement("div")
+
 // estilo da barrinha
 bar.style.height = "7px"
 bar.style.width = "700px"
@@ -9,4 +13,22 @@ bar.style.top = "0"
 bar.style.left = "0"
 bar.style.zIndex = "9999"
 
+// adicionar no corpo da página
 document.body.append(bar)
+
+// atualizar a barrinha
+function updateBar(){
+// o tamanho da caixa que contém o texto
+// verificar em que posição da página eu estou
+const textHeight = postWrap.offsetHeight
+// console.log(body.offsetHeight)
+const pagePositionY = window.pageXOffset
+// regra de 3
+const updateBar = pagePositionY * 100 / textHeight
+bar.style.width = updateBar + "%"
+}
+
+window.addEventListener("load", () => {
+// verificar o movimento do sroll
+document.addEventListener("scroll", updateBar)
+})
