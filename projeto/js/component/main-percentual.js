e946dfb17c3d8ebd6907b3543d0608698669cc79
mateusmacedo <mateusouza2014@live.com>
2021-02-22 00:26:27 -0300

    nova version do JS

--- projeto/js/component/main-percentual.js
+++ projeto/js/component/main-percentual.js
@@ -1,34 +1,31 @@
 // pegar o container do texto
-const body = document.querySelector(".body")
-
+ const wrapProjects = document.querySelector(".wrap-projects")
 // cria a barrinha
 let bar = document.createElement("div")
 
 // estilo da barrinha
-bar.style.height = "7px"
-bar.style.width = "700px"
+bar.style.height = "5px"
+bar.style.width = "0"
 bar.style.backgroundColor = "#6633cc"
 bar.style.position = "fixed"
 bar.style.top = "0"
 bar.style.left = "0"
 bar.style.zIndex = "9999"
+bar.style.transition = "0.2"
 
 // adicionar no corpo da página
 document.body.append(bar)
 
 // atualizar a barrinha
-function updateBar(){
-// o tamanho da caixa que contém o texto
-// verificar em que posição da página eu estou
-const textHeight = postWrap.offsetHeight
-// console.log(body.offsetHeight)
-const pagePositionY = window.pageXOffset
-// regra de 3
-const updateBar = pagePositionY * 100 / textHeight
-bar.style.width = updateBar + "%"
-}
 
-window.addEventListener("load", () => {
-// verificar o movimento do sroll
-document.addEventListener("scroll", updateBar)
+// verificar o movimento do scroll
+document.addEventListener("scroll", ()=> {
+// o tamanho da caixa que contém o texto
+const textHeight = wrapProjects.offsetHeight
+// verificar em que posição da página eu estou
+const pagePositionY = window.pageYOffset
+// regra de 3 simples
+const updateBar = pagePositionY * 100 / textHeight
+
+bar.style.width = updateBar + "%"
 })
