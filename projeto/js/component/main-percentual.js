c4aa782d064b4cd684f224af986e64c9a7d4cf81
mateusmacedo <mateusouza2014@live.com>
2021-02-21 21:44:38 -0300

    nova version do CSS

---
+++ projeto/js/component/main-percentual.js
@@ -0,0 +1,12 @@
+// cria a barrinha
+let bar = document.createElement("div")
+// estilo da barrinha
+bar.style.height = "7px"
+bar.style.width = "700px"
+bar.style.backgroundColor = "#6633cc"
+bar.style.position = "fixed"
+bar.style.top = "0"
+bar.style.left = "0"
+bar.style.zIndex = "9999"
+
+document.body.append(bar)
